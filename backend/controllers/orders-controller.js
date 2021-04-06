66524644a6b4eb919596f9cea240aeab53f53dd0
Eddie Nubes <cool.gegeg@gmail.com>
2021-04-06 22:57:33 +0300

    test commit to check auto-deploy on server 6

--- backend/controllers/orders-controller.js
+++ backend/controllers/orders-controller.js
@@ -22,7 +22,7 @@ const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const {validationResult} = require('express-validator');
 
-// i hate js so really really much
+// i hate js so really really really much. I'm monstrous!
 
 const createOrderWithPaypal = async (req, res, next) => {
     const errors = validationResult(req);
